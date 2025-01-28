2ac5615c7085f195b6135cf3139dfb92086203cd
Ira <dudairocka@gmail.com>
2025-01-28 20:40:52 +0700

    1 задание

--- chess.py
+++ chess.py
@@ -12,9 +12,28 @@ board= np.array([[1,0,1,0,1,0,1,0], [0,1,0,1,0,1,0,1], [1,0,1,0,1,0,1,0], [0,1,0
 mp.imshow(board,cmap='hot')
 mp.title("Chess board")
 mp.xticks(range(8) , labels=letters)
-mp.yticks(range(8),labels=[f'i' for i in range(1,9)])
-x=str(input("letter a-h : "))
-y = int(input("number 1-8 : "))
-circle = mp.Circle((x,y) , radius=(0.2))
+mp.yticks(range(8),labels=[f'{i}' for i in range(1,9)])
+x=ord(input("letter a-h : "))-96
+print(x)
+y = float(input("number 1-8 : "))
+circle = mp.Circle((x-1,y-1) , radius=(0.2), color = "pink")
+for ferz in range(8):
+    #up/down
+    if ferz != y-1:
+        circle1 = mp.Circle((x - 1, ferz), radius=(0.1), color="purple")
+        ax.add_patch(circle1)
+    if ferz != x-1:
+        circle2 = mp.Circle((ferz, y-1), radius=(0.1), color="purple")
+        ax.add_patch(circle2)
+    #diagonally
+    circle3 = mp.Circle((x - 1 + ferz, y-1 + ferz), radius=(0.1), color="purple")
+    ax.add_patch(circle3)
+    circle4 = mp.Circle((x - 1 - ferz, y - 1 + ferz), radius=(0.1), color="purple")
+    ax.add_patch(circle4)
+    circle5 = mp.Circle((x - 1 + ferz, y - 1 - ferz), radius=(0.1), color='purple')
+    ax.add_patch(circle5)
+    circle6 = mp.Circle((x - 1 - ferz, y - 1 - ferz), radius=(0.1), color="purple")
+    ax.add_patch(circle6)
+
 ax.add_patch(circle)
 mp.show()
