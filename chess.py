9c50aa8f4c19634f439f617a7d1739b187c6e5e9
Дуда Ирина Дмитриевна <i.duda@main.nsu.ru>
2025-02-01 12:01:57 +0700

    второе задание не полномтью

--- chess.py
+++ chess.py
@@ -14,9 +14,9 @@ mp.title("Chess board")
 mp.xticks(range(8) , labels=letters)
 mp.yticks(range(8),labels=[f'{i}' for i in range(1,9)])
 x=ord(input("letter a-h : "))-96
-print(x)
+#print(x)
 y = float(input("number 1-8 : "))
-circle = mp.Circle((x-1,y-1) , radius=(0.2), color = "pink")
+circle = mp.Circle((x-1,y-1) , radius=(0.2), color = 'blue')
 for ferz in range(8):
     #up/down
     if ferz != y-1:
